534b75eef1259bb75e55db6b3df5b5049cf60b3c
Scott Larson <lottscarson@gmail.com>
2024-11-21 23:39:14 -0700

    Updated RetrofitClient.kt to make a constant private

--- mobile/map-locations/android/app/src/main/java/com/example/maplocations/network/RetrofitClient.kt
+++ mobile/map-locations/android/app/src/main/java/com/example/maplocations/network/RetrofitClient.kt
@@ -5,7 +5,7 @@ import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "https://raw.githubusercontent.com/lottscarson/coding-exercises/master/mobile/map-locations/"
+private const val BASE_URL = "https://raw.githubusercontent.com/lottscarson/coding-exercises/master/mobile/map-locations/"
 
 object RetrofitClient {
     // Register the custom deserializer with Gson
